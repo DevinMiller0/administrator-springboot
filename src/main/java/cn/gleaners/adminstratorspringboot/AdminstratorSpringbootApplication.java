5ed844b586f34373edbeb00a31fdac259542ced5
wangdesheng <wangdesheng2017@163.com>
2020-06-12 16:41:36 +0800

    first..

---
+++ src/main/java/cn/gleaners/adminstratorspringboot/AdminstratorSpringbootApplication.java
@@ -0,0 +1,13 @@
+package cn.gleaners.adminstratorspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminstratorSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminstratorSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/gleaners/adminstratorspringboot/AdminstratorSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.gleaners.adminstratorspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminstratorSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
