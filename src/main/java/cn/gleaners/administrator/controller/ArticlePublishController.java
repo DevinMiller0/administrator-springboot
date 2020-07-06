1a06948edb3c9a7ae34017c1941241770e1fdd9c
wangdesheng <wangdesheng2017@163.com>
2020-07-06 18:31:29 +0800

    first..

---
+++ src/main/java/cn/gleaners/administrator/controller/ArticlePublishController.java
@@ -0,0 +1,18 @@
+package cn.gleaners.administrator.controller;
+
+import cn.gleaners.administrator.utils.response.ResultData;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author WangDeSheng
+ * @date 2020/7/6 15:19
+ * description：
+ */
+@Controller
+@RequestMapping("/publish")
+public class ArticlePublishController {
+
+//    public ResultData<Object> publish
+
+}
